53d681575288bc6a822c7e88960622daaffbc198
Amaljith Sivadas <sayhelloamal@gmail.com>
2023-05-15 14:16:34 +0530

    style update

--- js/script.js
+++ js/script.js
@@ -14,6 +14,23 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+
 // Nav open close
 const body = document.querySelector('body'),
       navMenu = body.querySelector('.menu-content'),
